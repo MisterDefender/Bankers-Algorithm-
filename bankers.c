9692956eab66dd4426963f7f446aa595457882e9
anandviplav <72974198+anandviplav@users.noreply.github.com>
2020-10-16 16:08:08 +0530

    Update bankers.c
    added definition of banker algorithm

--- bankers.c
+++ bankers.c
@@ -160,3 +160,5 @@ int main()
     	}
     	return 0;
 }
+/*** The banker’s algorithm is a resource allocation and deadlock avoidance algorithm that tests for safety by simulating the allocation for predetermined maximum possible amounts of all resources, 
+then makes an “s-state” check to test for possible activities, before deciding whether allocation should be allowed to continue.**/
